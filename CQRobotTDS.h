5833bceebc8d52cfd3f9f94541b007b80c00d1c3
redPanther <redPanther@users.noreply.github.com>
2023-07-30 00:09:10 +0200

    Update CQRobotTDS.h

--- CQRobotTDS.h
+++ CQRobotTDS.h
@@ -1,20 +1,3 @@
- 
-/***************************************************
- DFRobot Gravity: Analog TDS Sensor/Meter
- <https://www.dfrobot.com/wiki/index.php/Gravity:_Analog_TDS_Sensor_/_Meter_For_Arduino_SKU:_SEN0244>
- 
- ***************************************************
- This sample code shows how to read the tds value and calibrate it with the standard buffer solution.
- 707ppm(1413us/cm)@25^c standard buffer solution is recommended.
- 
- Created 2018-1-3
- By Jason <jason.ling@dfrobot.com@dfrobot.com>
- 
- GNU Lesser General Public License.
- See <http://www.gnu.org/licenses/> for details.
- All above must be included in any redistribution.
- ****************************************************/
-
 #ifndef CQROBOT_H
 #define CQROBOT_H
 
